e7559f59107bf366094791b88336bfe32bafef0a
Rayan Kobrossly <rayan.kobrossly3@etu.univ-lorraine.fr>
2025-11-12 16:12:33 +0100

    v2 service

--- Fetescience/src/main/java/com/example/fetescience/model/Atelier.java
+++ Fetescience/src/main/java/com/example/fetescience/model/Atelier.java
@@ -8,9 +8,12 @@ import java.util.*;
 
 @Entity
 public class Atelier {
+    @Getter
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id_atelier;
 
+    @Getter
+    @Setter
     @Column(nullable = false)
     private String titre;
 
@@ -34,28 +37,29 @@ public class Atelier {
         creneaux = new ArrayList<Creneau>();
     }
 
-
-    public Long getId() { System.out.println(id_atelier); return id_atelier; }
-    public String getTitre() {System.out.println(getTitre());   return titre; }
-    public List<Creneau> getCreneaux() {System.out.println(creneaux);  return creneaux;}
-
     // **************************************************
     // VERIFIER QUE LA LISTE N'EST PAS VIDE AVEC TRY ET CATCH ET THROWS PLUS TARD
     // **************************************************
-    public void setTitre(String titre) { System.out.println("title set"); this.titre = titre; }
+
 
     // ******************************* CRENEAUX
-    public void ajouterCreneau(Creneau c){System.out.println("creneaux set"); creneaux.add(c);}
+    public void ajouterCreneau(Creneau c){
+        System.out.println("creneaux set"); creneaux.add(c);
+        c.setAtelier(this);
+    }
+    public void supprimerCreneau(Creneau c){
+        if (c != null && creneaux.contains(c)){
+            System.out.println("creneaux deleted"); creneaux.remove(c);
+            c.setAtelier(null);
+        }
+    }
 
-    public void supprimerCreneau(int index){if (index >= 0 && index < creneaux.size()){ System.out.println("creneaux deleted"); creneaux.remove(index);}}
-    public void supprimerCreneau(Creneau c){ if (c != null && creneaux.contains(c)){System.out.println("creneaux deleted"); creneaux.remove(c);}}
-
-    public void modifierStatutCreneau(int index, boolean statut){if (index >= 0 && index < creneaux.size()){System.out.println("creneaux modifie");creneaux.get(index).setStatut(statut);}}
+    /*public void modifierStatutCreneau(int index, boolean statut){if (index >= 0 && index < creneaux.size()){System.out.println("creneaux modifie");creneaux.get(index).setStatut(statut);}}
     public void modifierlieu(int index, String lieu){if(index >= 0 && index < creneaux.size() && creneaux.get(index)!=null){System.out.println("creneaux modifie"); creneaux.get(index).setLieu(lieu);}}
     public void modifierduree(int index, int duree){if(index >= 0 && index < creneaux.size() && creneaux.get(index)!=null){System.out.println("creneaux modifie"); creneaux.get(index).setDuree(duree);}}
     public void modifierhoraire(int index, int horaire){if(index >= 0 && index < creneaux.size() && creneaux.get(index)!=null){System.out.println("creneaux modifie"); creneaux.get(index).setHoraire_debut(horaire);}}
-
-    public Creneau getCreneaux(int index) {
+*/
+    public Creneau getCreneau(int index) {
         if (index >= 0 && index < creneaux.size()) {
             System.out.println(creneaux.get(index));
             return creneaux.get(index);
@@ -63,12 +67,12 @@ public class Atelier {
             return null;
         }
     }
-    //public List<Creneau> getListCreneaux(){return creneaux;}
 
 
-    // ********************************** PARTICIPANT
+    /// ********************************** PARTICIPANT est géré depuis créneau
     //public void ajouterParticipant(Participant p){this.participant=p;}
    // public boolean verifiercompatibilite(int index, Participant part){return false;} /* ou Creneau c chepa au cas ou
+    // depuis SERVICE
 
     // ************************ Participant should be in creneaux not in atelier !!!!!!!
 
